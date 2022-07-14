f55f07b8b31440c3187b1940270c0f40d95e0108
OEUgwi06 <ugwioliver@gmail.com>
2022-07-14 06:46:21 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - Returns the length of a string
- * @str: The string to get the length of
+ * @s: The string to get the length of
  * Return: Length of the string
  */
 int _strlen(char *s)
