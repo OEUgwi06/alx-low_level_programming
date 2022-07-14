d655ed2402308712aefb16de9517ee0c3698a9c0
OEUgwi06 <ugwioliver@gmail.com>
2022-07-14 07:47:04 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,7 +3,7 @@
 /**
  * print_rev - Prints a string in reverse followed by a new line
  * @s: string to be printed in reverse
- * Return: Nothing 
+ * Return: Nothing
  */
 void print_rev(char *s)
 {
