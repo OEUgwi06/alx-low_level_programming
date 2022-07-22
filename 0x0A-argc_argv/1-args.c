68df27cedfe20995243f48c0fb8c13d0c91e8ee0
OEUgwi06 <ugwioliver@gmail.com>
2022-07-22 07:07:14 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: Arguments
+ * @argv: Array pointing to arguments
+ * Return: 0
+ */
+int main(int argc, _attribute_((unused)) char *argv[])
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+	}
+	printf("%d\n", a = 1);
+	return (0);
+}
