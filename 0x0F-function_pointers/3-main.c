26a7abb80b850afb96da03e73e8406dfd41900c9
OEUgwi06 <ugwioliver@gmail.com>
2022-08-05 04:47:50 -0700

    3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,16 +1,23 @@
 #include <stdlib.h>                                                                                                                                      
 #include <stdio.h>                                                                                                                                       
-#include <string.h>
 #include "3-calc.h" 
 
 /**
- * main - function that Prints result of an operation
- * @argc: Number of arguments
- * @argv: Value of arguments
- * Return: Returns an int.
+ * main - get_op_func has operators correlated with
+ * func signs and funcs from op_func
+ * if not 4 arguments, return Error & exit 98
+ * if op is null, return Error & exit 99
+ * if div or mod 0, return Error & exit 100
+ * run calc, input one, operator, input two = pointer res to get_op
+ * @argc: arguments
+ * @argv: double pointer to arguments
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
+	int one, two, ans;
+	int (*res)(int, int);
+	char *get_op;
 
 	if (argc != 4)
 	{
@@ -18,26 +25,26 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	if (
-			(strcmp(argv[2], "+") &&
-			 strcmp(argv[2], "-") &&
-			 strcmp(argv[2], "*") &&
-			 strcmp(argv[2], "/") &&
-			 strcmp(argv[2], "%"))
-	   )
+	one = atoi(argv[1]);
+	two = atoi(argv[3]);
+	get_op = argv[2];
+
+	/* added edge case if argv[2] was longer than 1 char*/
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
 
-	if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-			&& atoi(argv[3]) == 0)
+	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
+	res = get_op_func(get_op);
+	ans = res(one, two);
 
+	printf("%d\n", ans);
 	return (0);
 }
